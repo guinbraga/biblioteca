2a1e182f40d0c705a2d303b9c49412af3ebac6c7
Alexandre Marchioli <alexandresilva.marchioli@gmail.com>
2026-06-26 23:46:36 -0300

    Atualização mínima do LivroDAO - adicionando query personalizada (menor lentidão)

--- src/main/java/com/uel/biblioteca/repository/LivroDAO.java
+++ src/main/java/com/uel/biblioteca/repository/LivroDAO.java
@@ -3,6 +3,7 @@ package com.uel.biblioteca.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.uel.biblioteca.model.Livro;
@@ -13,5 +14,8 @@ public interface LivroDAO extends JpaRepository<Livro, Long> {
 	Livro findByCodigo(int codigo);
 	
 	List<Livro> findByTituloNomeContainingIgnoreCase(String nome);
+
+	@Query("SELECT l FROM Livro l LEFT JOIN FETCH l.titulo")
+	List<Livro> findAllComTitulo();
 
 }
